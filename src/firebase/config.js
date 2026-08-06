ac3017a4a32c4e863e8021ae8f788e618d064cc3
Raihan Ganhany <raihan@lpk-humaira.local>
2026-08-06 09:43:02 +0700

    fix: add fallback firebase config credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyDelNwl-q5tx6VJunEo4zVFuhv3vk8fHg',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'lpk-humaira-web.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'lpk-humaira-web',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'lpk-humaira-web.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '357193438990',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:357193438990:web:eaffe044ca5d9d8aefa95f',
 };
 
 const app = initializeApp(firebaseConfig);
